1699ffd83398182696eccf000df54985206a901d
luis-fernando-job <fernandoluis270@gmail.com>
2024-07-01 17:12:23 -0300

    iniciando-função-dos-botões

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
-const valorBtn = document.getElementsByClassName('btn-teclado')
+const input = document.querySelector('.input-active')
+const logo = document.querySelector('.logo-justica-titulo')
+const valorBtn = document.querySelector('.btn-teclado')
 
-function cliqueBtn (value) {
-    console.log(value)
+valorBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log('teste-active')
+})
+
+function sumirLogo () {
+    input.classList.remove('input-disabled')
+    logo.classList.add('logo-justica-titulo-disabled')
 }
+setTimeout(sumirLogo, 3000)
+
