87e7ebc5cdb7010d85a328a00eace73ea90d774e
luis-fernando-job <fernandoluis270@gmail.com>
2024-07-03 10:28:18 -0300

    projeto-finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,47 @@
-const input = document.querySelector('.input-active')
+const input = document.querySelector('.div-input')
 const logo = document.querySelector('.logo-justica-titulo')
 const inputValue = document.getElementById('input-value')
 const nomeAluno = document.getElementById('nome-aluno')
-const aluno = document.getElementsByName('aluno1')
+const anularVoto = document.getElementById('btn-branco')
+const corrigirVoto = document.getElementById('btn-corrigir')
+const confirmarVoto = document.getElementById('btn-confirmar')
+const text = document.getElementById('text')
 
 function mostrarValorBtn (num) {
     inputValue.value = num
 
     if (num === '1') {
-        nomeAluno.innerHTML = 'Nome da menina que não sei'
+        nomeAluno.innerHTML = 'Maria e Gabi'
     } else if (num === '2') {
-        nomeAluno.innerHTML = `Nome do menino que não sei`
-    }   
+        nomeAluno.innerHTML = `Lucas Kenji e Murilo`
+    } else (
+        nomeAluno.innerHTML = `Essa chapa não está mais competindo`
+    )
+}
+
+function anularInput() {
+    input.classList.add('input-disabled')
+    text.classList.remove('text-disabled')
+
+    function reload () {
+        window.location.reload(true)
+    }
+    setTimeout(reload, 2000)
+}
+
+function corrigirInput () {
+    nomeAluno.innerHTML = ``
+    inputValue.value = ``
+}
+
+function confirmarInput () {
+    input.classList.add('input-disabled')
+    text.classList.remove('text-disabled')
+
+    function reload () {
+        window.location.reload(true)
+    }
+    setTimeout(reload, 2000)
 }
 
 function sumirLogo () {
