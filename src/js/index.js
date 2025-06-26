94e51f5532e9093a685060e3baf91998b175e94c
Luis Fernando <fernandoluis270@gmail.com>
2025-06-25 21:17:38 -0400

    trocando nome da variavel

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('.div-input')
 const logo = document.querySelector('.logo-justica-titulo')
 const inputValue = document.getElementById('input-value')
-const nomeAluno = document.getElementById('nome-aluno')
+const chapa = document.getElementById('chapa')
 const anularVoto = document.getElementById('btn-branco')
 const corrigirVoto = document.getElementById('btn-corrigir')
 const confirmarVoto = document.getElementById('btn-confirmar')
@@ -12,13 +12,7 @@ function mostrarValorBtn (num) {
     inputValue.value = num
     audioPlay.play()
 
-    if (num === '1') {
-        nomeAluno.innerHTML = 'Maria e Gabi'
-    } else if (num === '2') {
-        nomeAluno.innerHTML = `Lucas Kenji e Murilo`
-    } else (
-        nomeAluno.innerHTML = `Essa chapa não está mais competindo`
-    )
+    chapa.innerHTML = 'Chapa ' + num
 }
 
 function anularInput() {
@@ -33,12 +27,20 @@ function anularInput() {
 }
 
 function corrigirInput () {
-    nomeAluno.innerHTML = ``
+    chapa.innerHTML = ``
     inputValue.value = ``
     audioPlay.play()
 }
 
 function confirmarInput () {
+    if(localStorage.getItem(`Chapa_${inputValue.value}`)) {
+
+        let count = localStorage.getItem(`Chapa_${inputValue.value}`)
+        localStorage.setItem(`Chapa_${inputValue.value}`, Number(count) + 1)
+        
+    } else {
+        localStorage.setItem(`Chapa_${inputValue.value}`, 1)
+    }
     input.classList.add('input-disabled')
     text.classList.remove('text-disabled')
     audioPlay.play()
